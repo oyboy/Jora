d700fe58d410a36e9939248dc332b171280bd87c
Hira <atom_box687@mail.ru>
2024-10-01 16:54:52 +0300

    Initializing project

---
+++ src/main/java/com/main/Jora/HomeController.java
@@ -0,0 +1,12 @@
+package com.main.Jora;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
---
+++ src/main/java/com/main/Jora/JoraApplication.java
@@ -0,0 +1,13 @@
+package com.main.Jora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoraApplication.class, args);
+	}
+
+}
