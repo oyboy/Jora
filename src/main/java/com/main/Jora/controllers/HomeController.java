7b3c74877a2c4ad9f4211a063d7004134efee3d7
Hira <atom_box687@mail.ru>
2024-10-05 15:11:04 +0300

    Added user registration

--- src/main/java/com/main/Jora/controllers/HomeController.java
+++ src/main/java/com/main/Jora/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.main.Jora.controllers;
 
 import com.main.Jora.models.Project;
+import com.main.Jora.models.User;
 import com.main.Jora.repositories.ProjectRepository;
 import com.main.Jora.services.ProjectService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +23,15 @@ public class HomeController {
     ProjectRepository projectRepository;
     @Autowired
     ProjectService projectService;
+
+    @ModelAttribute(name = "user")
+    public User getUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof User) {
+            return (User) authentication.getPrincipal();
+        }
+        return new User();
+    }
     @GetMapping
     public String home(Model model){
         Iterable<Project> projectList = projectRepository.findAll();
