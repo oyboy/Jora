18fe36b1e86221ce68cb304b24edf2c1668114df
oyboy <atom_box687@mail.ru>
2024-12-06 19:38:15 +0000

    Configured logs for sending to kafka

---
+++ src/main/java/com/main/Jora/loger/KafkaProducerConfig.java
@@ -0,0 +1,36 @@
+package com.main.Jora.loger;
+
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+    @Value("${spring.kafka.bootstrap-servers[0]:localhost:9092}")
+    private String kafkaServerPort;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        System.out.println("Kafka server: " + kafkaServerPort);
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerPort);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
